7411b640ef998518c29feb9f4a383a1230ee7bca
gorkalertxundi <gorka.lertxundi@alumni.mondragon.edu>
2022-03-26 12:47:13 +0100

    first commit

---
+++ src/main/java/lertxundi/gorka/katualai/form/CatInformForm.java
@@ -0,0 +1,33 @@
+package lertxundi.gorka.katualai.form;
+
+import groovy.transform.Generated;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Generated
+@Getter
+@Setter
+public class CatInformForm {
+    private String sarreraData;
+    private String irteeraData;
+    private String sexua;
+    private String adina;
+    private String ilea;
+    private String arraza;
+    private String koloniaKokapena;
+    private String katuArgazkia;
+    private Boolean ovarioHisterectomia;
+    private Boolean ovariectomia;
+    private Boolean orquiectomia;
+    private Boolean belarriEzkerrean;
+    private Boolean belarriEskuinean;
+    private Boolean gaixotasunBai;
+    private Boolean gaixotasunEz;
+    private String lorratzenDeskribapena;
+    private Boolean bizitzekoGai;
+    private Boolean bizitzekoEzGai;
+    private Boolean eutanasiatua;
+    private String arrazonamendua;
+}
---
+++ src/test/java/lertxundi/gorka/katualai/KatualaiApplicationTests.java
@@ -0,0 +1,13 @@
+package lertxundi.gorka.katualai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatualaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
